b067dae2518a5f97272b6ce921818569916f9ef2
GitHub Action <action@github.com>
2022-08-20 15:44:22 +0000

    Template cleanup

--- src/main/kotlin/com/github/stkale/shinyeureka/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/stkale/shinyeureka/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.stkale.shinyeureka.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.stkale.shinyeureka.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/stkale/shinyeureka/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stkale/shinyeureka/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stkale.shinyeureka.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stkale.shinyeureka.MyBundle
 
 class MyApplicationService {
 
